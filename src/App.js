b9a00ef78a9759f5104ab58ad38d8bc235f09363
John Cuviello <jcuviell@uci.edu>
2022-10-20 19:49:28 -0700

    added colors

--- src/App.js
+++ src/App.js
@@ -15,13 +15,29 @@ const Project = ({title, image, alt}) => {
   )
 }
 
-
 const App = () => (
-  <div>
-    
-    <Spline id="face" scene="https://prod.spline.design/wDQw1bMkAQ9L5avp/scene.splinecode" />
-    
-  </div>
+  <>
+    <h1>It's-a me, John.</h1>
+    <div id="face">
+      <Spline scene="https://prod.spline.design/wDQw1bMkAQ9L5avp/scene.splinecode" />
+    </div>
+    <div id="projects">
+      <h2>Projects</h2>
+      <h3>Project 1</h3>
+      <p>My first projects
+      Ya
+      He
+      Bleh</p>
+
+      <h3>Project 2</h3>
+      <p>My second projects
+      Ya
+      He
+      Bleh</p>
+
+    </div>
+  </>
+
 )
 
 export default App;
