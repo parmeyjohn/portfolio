e274f27876a1c76c96f3afd77afad6bc99e1914f
Nick Cuviello <nickcuv@gmail.com>
2022-10-21 01:07:56 -0700

    added grid

--- src/App.js
+++ src/App.js
@@ -17,25 +17,19 @@ const Project = ({title, image, alt}) => {
 
 const App = () => (
   <>
-    <h1>It's-a me, John.</h1>
-    <div id="face">
-      <Spline scene="https://prod.spline.design/wDQw1bMkAQ9L5avp/scene.splinecode" />
+    
+    <div className="grid">
+      <div></div>
+      <div className="grid-col">
+        Hey
+      </div>
+      <div className="face">
+        <Spline scene="https://prod.spline.design/wDQw1bMkAQ9L5avp/scene.splinecode" />
+      </div>
     </div>
-    <div id="projects">
-      <h2>Projects</h2>
-      <h3>Project 1</h3>
-      <p>My first projects
-      Ya
-      He
-      Bleh</p>
-
-      <h3>Project 2</h3>
-      <p>My second projects
-      Ya
-      He
-      Bleh</p>
-
-    </div>
+    
+  <div className="space"></div>
+  
   </>
 
 )
