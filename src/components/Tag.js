07d2d0e0340fda5118b7b8bc1834e9540e958405
John Cuviello <jcuviell@uci.edu>
2023-07-17 23:56:58 -0700

    added basic tag component

---
+++ src/components/Tag.js
@@ -0,0 +1,10 @@
+const Experience = ({ text }) => {
+    return (
+      <div className="bg-blue-300 border-b-2 border-blue-600 text-blue-500 hover:bg-blue-400">
+        {text}
+      </div>
+    );
+  };
+  
+  export { Experience };
+  
