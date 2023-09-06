ccad60e4e162d45dba2cc965388f1a740e666750
John Cuviello <jcuviell@uci.edu>
2023-09-06 16:56:31 -0700

    fixed about section formatting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import { FaLinkedin, FaGithub, FaInstagram, Fa } from "react-icons/fa";
 import { Experience } from "./components/Experience";
 import { Project } from "./components/Project";
+import { useState } from "react";
 
 const App = () => {
+  const [email, setEmail] = useState('johncuviello99@gmail.com')
+  
   return (
     <div className="h-auto w-full w-2xl bg-gradient-to-b from-indigo-950 to-slate-950">
       <nav className=" md:flex text-indigo-200 text-lg sticky h-20 w-full max-w-2xl p-4 mx-auto flex justify-between items-center">
-        <a className="whitespace-nowrap p-1">John Cuviello</a>
+        <p className="whitespace-nowrap p-1">John Cuviello</p>
         <div className="flex justify-end items-center">
-        <div className="flex invisible">
-          <a href='#about' className="p-2 hover:underline hover:text-indigo-200 transition-all ">About</a>
-          <a href='#projects' className="p-2 hover:underline hover:text-indigo-200 transition-all ">Projects</a>
-          <a href='#experience' className="p-2 hover:underline hover:text-indigo-200 transition-all ">Experience</a>
-          <a href='#contact' className="p-2 hover:underline hover:text-indigo-200 transition-all ">Contact Me</a>
-        </div>
+          <div className="flex">
+            <a
+              href="#about"
+              className="p-2 hover:underline hover:text-indigo-200 transition-all "
+            >
+              About
+            </a>
+            <a
+              href="#projects"
+              className="p-2 hover:underline hover:text-indigo-200 transition-all "
+            >
+              Projects
+            </a>
+            <a
+              href="#experience"
+              className="p-2 hover:underline hover:text-indigo-200 transition-all "
+            >
+              Experience
+            </a>
+            <a
+              href="#contact"
+              className="p-2 hover:underline hover:text-indigo-200 transition-all "
+            >
+              Contact
+            </a>
+          </div>
         </div>
       </nav>
 
@@ -31,13 +54,15 @@ const App = () => {
             <p className="text-3xl font-medium text-blue-700 pb-2">
               John Cuviello
             </p>
-            <p className=" text-center text-slate-400">
-              Web Developer
-            </p>
-            
+            <p className=" text-center text-slate-400">Web Developer</p>
+
             <div className="flex justify-around items-center my-8 h-fit">
-              <button className="rounded-full border-2 border-emerald-600 bg-emerald-400 hover:bg-emerald-500 transition-all duration-75 p-2 px-4">Resume</button>
-              <button className="rounded-full border-2 border-emerald-600 bg-emerald-400 hover:bg-emerald-500 transition-all duration-75 p-2 px-4">Contact Me</button>
+              <button className="rounded-full border-2 border-emerald-600 bg-emerald-400 hover:bg-emerald-500 transition-all duration-75 p-2 px-4">
+                Resume
+              </button>
+              <button className="rounded-full border-2 border-emerald-600 bg-emerald-400 hover:bg-emerald-500 transition-all duration-75 p-2 px-4">
+                Contact Me
+              </button>
             </div>
             <div className="flex justify-around m-2 h-fit">
               <a
@@ -56,12 +81,15 @@ const App = () => {
           </div>
         </div>
 
-        <div id='about' className="flex flex-col md:flex-row justify-between w-full h-auto my-24">
+        <div
+          id="about"
+          className="flex flex-col md:flex-row justify-between w-full h-auto my-24"
+        >
           <div className="bg-blue-300 text-center w-60 h-fit rounded-xl p-4 mx-auto sticky border-b-4 top-72 border-blue-600">
             <h2 className="text-lg ">About</h2>
           </div>
           <div className="h-auto w-[90%] md:w-80 mx-auto z-10">
-            <div className="bg-slate-200 border-b-8 border-b-indigo-400 w-full rounded-xl p-4 py-16 h-fit">
+            <div className="bg-slate-200 border-b-8 border-b-indigo-400 w-full rounded-xl p-4 h-fit">
               <ul className="ml-2">
                 <li className="p-2">I'm a dev and artist based in CA.</li>
                 <li className="p-2">
@@ -69,46 +97,51 @@ const App = () => {
                   and am looking for new opportunities.
                 </li>
                 <li className="p-2">
-                  I mostly use JavaScript and Python for web
-                  development, but I'm always willing to learn new languages and
-                  tech.
+                  I've mostly been doing React development, but
+                  I'm always willing to learn new languages and tech.
+                </li>
+                <li className="p-2">
+                  
                 </li>
                 <li className="p-2">
                   Feel free to check out my projects down below or contact me on
-                  my socials.
+                  my socials!
                 </li>
               </ul>
             </div>
           </div>
         </div>
 
-        <div id='projects' className="flex flex-col md:flex-row justify-between w-full h-auto">
+        <div
+          id="projects"
+          className="flex flex-col md:flex-row justify-between w-full h-auto pb-24"
+        >
           <div className="bg-blue-300 w-60 h-fit rounded-xl p-4 mx-auto text-center sticky top-72 border-b-4 border-blue-600">
             <h2 className="text-lg">Projects</h2>
           </div>
           <div className="h-full w-[90%] md:w-80 mx-auto z-10">
             <div className="mb-16">
-            <Project
-              title={"Cooldown"}
-              link={"usecooldown.com"}
-              bullets={[
-                "Constructed a responsive front-end to display journal entries using React and Tailwind CSS",
-                "Developed a back-end REST API with Node, Express, and MongoDB Atlas",
-                "Authenticated users via JSON web tokens, password hashing, and protected React Router client-side routes",
-                "Consumed external API to populate images and titles based on user search results",
-                "Applied end-to-end, integration, and unit testing with Cypress and Jest",
-              ]}
-              tags={[
-                "React",
-                "JavaScript",
-                "TailwindCSS",
-                "Node",
-                "Express",
-                "MongoDB",
-                "Cypress",
-                "Jest",
-              ]}
-            ></Project>
+              <Project
+                title={"Cooldown"}
+                link={"usecooldown.com"}
+                bullets={[
+                  "Constructed a responsive front-end to display journal entries using React and Tailwind CSS",
+                  "Developed a back-end REST API with Node, Express, and MongoDB Atlas",
+                  "Authenticated users via JSON web tokens, password hashing, and protected React Router client-side routes",
+                  "Consumed external API to populate images and titles based on user search results",
+                  "Applied end-to-end, integration, and unit testing with Cypress and Jest",
+                ]}
+                tags={[
+                  "React",
+                  "JavaScript",
+                  "TailwindCSS",
+                  "Node",
+                  "Express",
+                  "MongoDB",
+                  "Cypress",
+                  "Jest",
+                ]}
+              ></Project>
             </div>
             <Project
               title={"Fullstack E-commerce Site"}
@@ -130,19 +163,15 @@ const App = () => {
           </div>
         </div>
 
-        <div id='experience' className="w-full flex flex-col h-auto my-24">
-          <div className="bg-blue-300 text-center w-60 h-fit rounded-xl p-4 mx-auto md:m-8 sticky top-72 border-b-4 border-blue-600 z-10">
+        <div
+          id="experience"
+          className="flex flex-col md:flex-row justify-between w-full h-auto"
+        >
+          <div className="bg-blue-300 w-60 h-fit rounded-xl p-4 mx-auto text-center sticky top-72 border-b-4 border-blue-600">
             <h2 className="text-lg">Experience</h2>
           </div>
-
-          <div className="flex flex-col md:flex-row justify-between w-full h-auto">
-            <div className="w-60 flex mx-auto justify-center md:justify-end sticky top-[22.5rem] z-10 md:z-0">
-              <div className="bg-blue-600 w-fit p-4 rounded-xl">
-                <h2 className="">2022</h2>
-              </div>
-            </div>
-            <div className="h-full w-[90%] md:w-80 mx-auto z-20 mb-24">
-              <div className="mb-16">
+          <div className="h-full w-[90%] md:w-80 mx-auto z-10">
+            <div className="mb-16">
               <Experience
                 title={"B.S in Computer Science"}
                 company={"UCI"}
@@ -151,11 +180,11 @@ const App = () => {
                   "Data Structure Implementation and Analysis",
                   "Design and Analysis of Algorithms",
                   "Principles in System Design",
-                  "Data Management",
                   "Machine Learning and Data Mining",
                 ]}
               ></Experience>
-              </div>
+            </div>
+            <div className="mb-16">
               <Experience
                 title={"Software Engineer Capstone"}
                 company={"Knowde"}
@@ -165,16 +194,7 @@ const App = () => {
                 ]}
               ></Experience>
             </div>
-          </div>
-
-          <div className="flex flex-col items-center md:flex-row justify-between w-full h-auto">
-          <div className="w-60 flex mx-auto my-2 justify-center md:justify-end sticky top-[22.5rem] z-10 md:z-0">
-              <div className="bg-blue-600 w-fit p-4 rounded-xl ">
-                <h2 className="">2021</h2>
-              </div>
-            </div>
-            <div className="h-full w-[90%] md:w-80 mx-auto z-20">
-              <div className="mb-16">
+            <div className="mb-16">
               <Experience
                 title={"Team Member"}
                 company={"Chipotle"}
@@ -183,29 +203,31 @@ const App = () => {
                   "Provided exemplary service to over 150 customers per shift",
                 ]}
               ></Experience>
-              </div>
-              <Experience
-                title={"Sales Associate"}
-                company={"Vans"}
-                bullets={[
-                  "Worked the shoe floor, cash register, and apparel deck engaging with customers and assisting coworkers",
-                  "Memorized dozens of shoe and apparel styles to improve ability to meet customers’ needs",
-                ]}
-              ></Experience>
             </div>
+            <Experience
+              title={"Sales Associate"}
+              company={"Vans"}
+              bullets={[
+                "Worked the shoe floor, cash register, and apparel deck engaging with customers and assisting coworkers",
+                "Memorized dozens of shoe and apparel styles to improve ability to meet customers’ needs",
+              ]}
+            ></Experience>
           </div>
         </div>
 
-        <div id='contact' className="flex flex-col md:flex-row justify-between w-full h-screen">
+        <div
+          id="contact"
+          className="flex flex-col md:flex-row justify-between w-full h-screen mt-24"
+        >
           <div className="bg-blue-300 w-60 h-fit rounded-xl p-4 text-center mx-auto sticky top-72 border-b-4 border-blue-600">
-            <h2  className="text-lg ">Contact</h2>
+            <h2 className="text-lg ">Contact</h2>
           </div>
           <div className="h-screen w-[90%] mx-auto md:w-80 flex z-10">
             <div className="bg-slate-200 border-b-8 border-b-indigo-400 rounded-xl p-4 h-fit">
               <p>
-                LinkedIn or Email are the most reliable ways to contact me. I
-                would add other socials but I don't use them
+                Email is my preferred method of contact:
               </p>
+              <a href = {`mailto: ${email}`}>Send Email</a>
             </div>
           </div>
         </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,8 +1,8 @@
 const Experience = ({ title, company, subtitle, bullets }) => {
   return (
-    <div className="bg-slate-200 border-b-8 border-b-indigo-400 rounded-xl p-4 h-fit">
-      <h1 className="text-[1.0rem] text-blue-700 font-semibold">{title} </h1>
-      <h2 className="text-[0.8rem]">@ {company}</h2>
+    <div className="bg-slate-200 border-b-8 border-b-indigo-400 rounded-xl p-8 h-fit">
+      <h1 className="text-lg text-blue-700 font-semibold">{title} </h1>
+      <h2 className="mb-2">{company}</h2>
       <ul className="">
         {subtitle && <h2 className="mb-1">{subtitle}</h2>}
         {bullets.map((b) => (
